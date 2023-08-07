d073a94257ed5c61279766bce539ae0c0e045b2c
nadavgu <nadavgu@gmail.com>
2023-08-08 02:18:41 +0300

    Added API function to run in java process

--- matilda/matilda.py
+++ matilda/matilda.py
@@ -1,3 +1,4 @@
+from matilda.java_process_matilda_runner import JavaProcessMatildaRunner
 from matilda.matilda_process import MatildaProcess
 from matilda.matilda_runner import MatildaRunner
 
@@ -7,3 +8,6 @@ class Matilda:
     def run(runner: MatildaRunner) -> MatildaProcess:
         runner.run()
         return MatildaProcess()
+
+    def run_in_java_process(self) -> MatildaProcess:
+        return self.run(JavaProcessMatildaRunner())
