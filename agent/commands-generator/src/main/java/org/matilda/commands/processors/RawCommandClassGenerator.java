08e82ac73d51ea8ebf081c44e40d3520a3a4c749
nadavgu <nadavgu@gmail.com>
2023-08-10 06:58:01 +0300

    Added run method to generated raw command

--- agent/commands-generator/src/main/java/org/matilda/commands/processors/RawCommandClassGenerator.java
+++ agent/commands-generator/src/main/java/org/matilda/commands/processors/RawCommandClassGenerator.java
@@ -7,6 +7,7 @@ import org.matilda.commands.names.NameGenerator;
 
 import javax.annotation.processing.Filer;
 import javax.inject.Inject;
+import javax.lang.model.element.Modifier;
 import java.io.IOException;
 
 public class RawCommandClassGenerator implements Processor<CommandInfo> {
@@ -19,6 +20,7 @@ public class RawCommandClassGenerator implements Processor<CommandInfo> {
     @Inject
     public RawCommandClassGenerator() {}
 
+    private static final ArrayTypeName BYTE_ARRAY_TYPE_NAME = ArrayTypeName.of(TypeName.BYTE);
     private static final String SERVICE_FIELD_NAME = "mService";
 
     @Override
@@ -38,6 +40,7 @@ public class RawCommandClassGenerator implements Processor<CommandInfo> {
                 .addSuperinterface(Command.class)
                 .addField(createServiceField(command))
                 .addMethod(createInjectConstructor())
+                .addMethod(createRunMethod(command))
                 .build();
     }
 
@@ -50,6 +53,28 @@ public class RawCommandClassGenerator implements Processor<CommandInfo> {
     private MethodSpec createInjectConstructor() {
         return MethodSpec.constructorBuilder()
                 .addAnnotation(Inject.class)
+                .build();
+    }
+
+    private final static String RAW_PARAMETER_NAME = "rawParameter";
+    private final static String PARSED_PARAMETER_NAME = "parameter";
+    private final static String RETURN_VALUE_NAME = "returnValue";
+    private final static String EXCEPTION_NAME = "exception";
+
+    private MethodSpec createRunMethod(CommandInfo command) {
+        return MethodSpec.methodBuilder("run")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(ParameterSpec.builder(BYTE_ARRAY_TYPE_NAME, RAW_PARAMETER_NAME).build())
+                .returns(ArrayTypeName.of(TypeName.BYTE))
+                .beginControlFlow("try")
+                .addStatement("$T $L = $T.parseFrom($L)", command.parameterType(), PARSED_PARAMETER_NAME,
+                        command.parameterType(), RAW_PARAMETER_NAME)
+                .addStatement("$T $L = $L.$L($L)", command.returnType(), RETURN_VALUE_NAME,
+                        SERVICE_FIELD_NAME, command.name(), PARSED_PARAMETER_NAME)
+                .addStatement("return $L.toByteArray()", RETURN_VALUE_NAME)
+                .nextControlFlow("catch ($T $L)", IOException.class, EXCEPTION_NAME)
+                .addStatement("throw new $T($L)", RuntimeException.class, EXCEPTION_NAME)
+                .endControlFlow()
                 .build();
     }
 }
