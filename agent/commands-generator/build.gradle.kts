69d9e7223ff1c5711d37617b84aca39faa597aa8
nadavgu <nadavgu@gmail.com>
2023-08-11 22:48:32 +0300

    Added kapt

--- agent/commands-generator/build.gradle.kts
+++ agent/commands-generator/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("java")
     kotlin("jvm") version "1.9.0"
+    kotlin("kapt") version "1.9.0"
 }
 
 group = "org.matilda"
@@ -16,6 +17,7 @@ dependencies {
     implementation(project(":commands-generator-api"))
     implementation("com.google.dagger:dagger:2.47")
     annotationProcessor("com.google.dagger:dagger-compiler:2.47")
+    kapt("com.google.dagger:dagger-compiler:2.47")
     implementation("com.squareup:javapoet:1.10.0")
     implementation("org.apache.commons:commons-lang3:3.12.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
