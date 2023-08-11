57610496ca9fc5e8873ba73494eefff9118f2c4f
nadavgu <nadavgu@gmail.com>
2023-08-12 00:03:01 +0300

    Converted NameGenerator to kotlin

--- agent/commands-generator/src/main/java/org/matilda/commands/names/NameGenerator.kt
+++ agent/commands-generator/src/main/java/org/matilda/commands/names/NameGenerator.kt
@@ -1,111 +1,82 @@
-package org.matilda.commands.names;
+package org.matilda.commands.names
 
-import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.TypeName;
-import org.apache.commons.lang3.StringUtils;
-import org.matilda.commands.info.CommandInfo;
-import org.matilda.commands.info.ServiceInfo;
-import org.matilda.commands.python.PythonProperties;
-import org.matilda.commands.utils.Package;
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.TypeName
+import org.apache.commons.lang3.StringUtils
+import org.matilda.commands.info.CommandInfo
+import org.matilda.commands.info.ServiceInfo
+import org.matilda.commands.names.NameGenerator.ServiceNameGenerator.CommandNameGenerator
+import org.matilda.commands.python.PythonProperties
+import org.matilda.commands.utils.Package
+import org.matilda.commands.utils.Package.Companion.fromString
+import org.matilda.commands.utils.Package.Companion.joinPackages
+import javax.inject.Inject
 
-import javax.inject.Inject;
-import java.util.stream.Collectors;
-
-public class NameGenerator {
+class NameGenerator @Inject internal constructor() {
     @Inject
-    NameGenerator() {}
+    lateinit var mPythonProperties: PythonProperties
+    val pythonGeneratedCommandsPackage: Package
+        get() = mPythonProperties.pythonGeneratedPackage.subpackage("commands")
 
-    @Inject
-    PythonProperties mPythonProperties;
+    inner class ServiceNameGenerator(private val mServiceInfo: ServiceInfo) {
+        private val fullNamePackage: Package
+            get() = fromString(mServiceInfo.fullName)
+        val serviceClassName: String
+            get() = fullNamePackage.lastPart
+        private val serviceSnakeCaseName: String
+            get() {
+                val regex = "([a-z])([A-Z]+)".toRegex()
 
-    public static final Package MAIN_GENERATED_PACKAGE = Package.fromString("org.matilda.generated");
-    public static final Package COMMANDS_GENERATED_PACKAGE = MAIN_GENERATED_PACKAGE.subpackage( "commands");
-    public static final Package RAW_COMMAND_CLASSES_PACKAGE = COMMANDS_GENERATED_PACKAGE.subpackage( "raw");
-    public static final String COMMANDS_MODULE_CLASS_NAME = "CommandsModule";
-    public static final TypeName COMMANDS_MODULE_TYPE_NAME = ClassName.get(COMMANDS_GENERATED_PACKAGE.getPackageName(),
-            COMMANDS_MODULE_CLASS_NAME);
-    public static final String SERVICES_MODULE_CLASS_NAME = "ServicesModule";
-    public static final TypeName SERVICES_MODULE_TYPE_NAME = ClassName.get(COMMANDS_GENERATED_PACKAGE.getPackageName(),
-            SERVICES_MODULE_CLASS_NAME);
-    public static final Package ORIGINAL_PACKAGE = new Package("org", "matilda", "commands");
+                // Replacement string
+                val replacement = "$1_$2"
 
-    public Package getPythonGeneratedCommandsPackage() {
-        return mPythonProperties.pythonGeneratedPackage().subpackage("commands");
-    }
-
-    public class ServiceNameGenerator {
-        private final ServiceInfo mServiceInfo;
-
-        public ServiceNameGenerator(ServiceInfo serviceInfo) {
-            mServiceInfo = serviceInfo;
-        }
-
-        private Package getFullNamePackage() {
-            return Package.fromString(mServiceInfo.getFullName());
-        }
-
-        public String getServiceClassName() {
-            return getFullNamePackage().getLastPart();
-        }
-
-        public String getServiceSnakeCaseName() {
-            String regex = "([a-z])([A-Z]+)";
-
-            // Replacement string
-            String replacement = "$1_$2";
-
-            // Replace the given regex
-            // with replacement string
-            // and convert it to lower case.
-            return getServiceClassName()
-                    .replaceAll(regex, replacement)
-                    .toLowerCase();
-        }
-
-        public Package getPythonGeneratedServicePackage() {
-            return getPythonGeneratedCommandsPackage().subpackage(getServiceSnakeCaseName());
-        }
-
-        public Package getPackage() {
-            return getFullNamePackage().withoutLastPart();
-        }
-
-        private Package getServiceRelativePackage() {
-            return getPackage().removeCommonPrefixFrom(ORIGINAL_PACKAGE);
-        }
-
-        public class CommandNameGenerator {
-            private final CommandInfo mCommandInfo;
-
-            public CommandNameGenerator(CommandInfo commandInfo) {
-                mCommandInfo = commandInfo;
+                // Replace the given regex
+                // with replacement string
+                // and convert it to lower case.
+                return serviceClassName.replace(regex, replacement).lowercase()
             }
+        val pythonGeneratedServicePackage: Package
+            get() = pythonGeneratedCommandsPackage.subpackage(serviceSnakeCaseName)
+        private val servicePackage: Package
+            get() = fullNamePackage.withoutLastPart()
+        private val serviceRelativePackage: Package
+            get() = servicePackage.removeCommonPrefixFrom(ORIGINAL_PACKAGE)
 
-            public String getRawCommandClassName() {
-                return getServiceClassName() + StringUtils.capitalize(mCommandInfo.getName()) + "Command";
-            }
-
-            public String getRawCommandPackageName() {
-                return Package.joinPackages(RAW_COMMAND_CLASSES_PACKAGE, getServiceRelativePackage()).getPackageName();
-            }
-
-            public TypeName getRawCommandTypeName() {
-                return ClassName.get(getRawCommandPackageName(), getRawCommandClassName());
-            }
-
-            public String getFullCommandName() {
-                return getServiceRelativePackage().getParts().stream()
-                        .map(StringUtils::capitalize)
-                        .collect(Collectors.joining()) + getRawCommandClassName();
-            }
+        inner class CommandNameGenerator(private val mCommandInfo: CommandInfo) {
+            val rawCommandClassName: String
+                get() = serviceClassName + StringUtils.capitalize(mCommandInfo.name) + "Command"
+            val rawCommandPackageName: String
+                get() = joinPackages(RAW_COMMAND_CLASSES_PACKAGE, serviceRelativePackage).packageName
+            val rawCommandTypeName: TypeName
+                get() = ClassName.get(rawCommandPackageName, rawCommandClassName)
+            val fullCommandName: String
+                get() = serviceRelativePackage.parts.joinToString { str -> StringUtils.capitalize(str) } +
+                        rawCommandClassName
         }
     }
 
-    public ServiceNameGenerator forService(ServiceInfo serviceInfo) {
-        return new ServiceNameGenerator(serviceInfo);
+    fun forService(serviceInfo: ServiceInfo): ServiceNameGenerator {
+        return ServiceNameGenerator(serviceInfo)
     }
 
-    public ServiceNameGenerator.CommandNameGenerator forCommand(CommandInfo commandInfo) {
-        return forService(commandInfo.getService()).new CommandNameGenerator(commandInfo);
+    fun forCommand(commandInfo: CommandInfo): CommandNameGenerator {
+        return forService(commandInfo.service).CommandNameGenerator(commandInfo)
+    }
+
+    companion object {
+        private val MAIN_GENERATED_PACKAGE = fromString("org.matilda.generated")
+        val COMMANDS_GENERATED_PACKAGE = MAIN_GENERATED_PACKAGE.subpackage("commands")
+        val RAW_COMMAND_CLASSES_PACKAGE = COMMANDS_GENERATED_PACKAGE.subpackage("raw")
+        const val COMMANDS_MODULE_CLASS_NAME = "CommandsModule"
+        val COMMANDS_MODULE_TYPE_NAME: TypeName = ClassName.get(
+            COMMANDS_GENERATED_PACKAGE.packageName,
+            COMMANDS_MODULE_CLASS_NAME
+        )
+        const val SERVICES_MODULE_CLASS_NAME = "ServicesModule"
+        val SERVICES_MODULE_TYPE_NAME: TypeName = ClassName.get(
+            COMMANDS_GENERATED_PACKAGE.packageName,
+            SERVICES_MODULE_CLASS_NAME
+        )
+        val ORIGINAL_PACKAGE = Package("org", "matilda", "commands")
     }
 }
