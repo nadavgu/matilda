d924305a998fce22dca2fffda607e81577e39b82
nadavgu <nadavgu@gmail.com>
2023-08-22 17:59:36 +0300

    Simplified java method stuff

--- agent/library/src/main/java/org/matilda/services/reflection/ReflectionService.java
+++ agent/library/src/main/java/org/matilda/services/reflection/ReflectionService.java
@@ -2,12 +2,10 @@ package org.matilda.services.reflection;
 
 import org.matilda.commands.MatildaCommand;
 import org.matilda.commands.MatildaService;
-import org.matilda.services.reflection.protobuf.Method;
-import org.matilda.services.reflection.protobuf.MethodSpec;
 import org.matilda.services.reflection.protobuf.ParameterType;
 
 import javax.inject.Inject;
-import java.lang.reflect.Parameter;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,24 +31,28 @@ public class ReflectionService {
     }
 
     @MatildaCommand
-    public List<org.matilda.services.reflection.protobuf.Method> getClassMethods(long id) {
+    public List<Long> getClassMethods(long id) {
         Class<?> clazz = (Class<?>) mObjectRepository.get(id);
-        return Arrays.stream(clazz.getDeclaredMethods()).map(method -> {
-            MethodSpec.Builder methodSpecBuilder = MethodSpec.newBuilder()
-                    .setName(method.getName());
-            for (Parameter parameter : method.getParameters()) {
-                ParameterType.Builder parameterTypeBuilder = ParameterType.newBuilder();
-                if (parameter.getType().isPrimitive()) {
-                    parameterTypeBuilder.setPrimitiveClassName(parameter.getType().getCanonicalName());
-                } else {
-                    parameterTypeBuilder.setClassId(mObjectRepository.add(parameter.getType()));
-                }
-                methodSpecBuilder.addParameterTypes(parameterTypeBuilder);
+        return Arrays.stream(clazz.getDeclaredMethods()).map(mObjectRepository::add).collect(Collectors.toList());
+    }
+
+    @MatildaCommand
+    public String getMethodName(long id) {
+        Method method = (Method) mObjectRepository.get(id);
+        return method.getName();
+    }
+
+    @MatildaCommand
+    public List<ParameterType> getMethodParameterTypes(long id) {
+        Method method = (Method) mObjectRepository.get(id);
+        return Arrays.stream(method.getParameters()).map(parameter -> {
+            ParameterType.Builder parameterTypeBuilder = ParameterType.newBuilder();
+            if (parameter.getType().isPrimitive()) {
+                parameterTypeBuilder.setPrimitiveClassName(parameter.getType().getCanonicalName());
+            } else {
+                parameterTypeBuilder.setClassId(mObjectRepository.add(parameter.getType()));
             }
-            return Method.newBuilder()
-                    .setMethodId(mObjectRepository.add(method))
-                    .setSpec(methodSpecBuilder)
-                    .build();
+            return parameterTypeBuilder.build();
         }).collect(Collectors.toList());
     }
 }
