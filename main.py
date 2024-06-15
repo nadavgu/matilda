ce0a389cd56d40bea5e854546221f296f1496f04
nadavgu <nadavgu@gmail.com>
2024-06-15 21:12:04 +0300

    Testing new API

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from matilda.matilda import Matilda
 
 if __name__ == '__main__':
     with Matilda().run_in_java_process(java_path='/home/user/Downloads/jre1.8.0_411/bin/java') as matilda_process:
-        print(matilda_process.java.find_class("java.lang.Math").get_method("abs", JavaPrimitiveType.INT).invoke_static(-3))
+        integer_class = matilda_process.java.find_class("java.lang.Integer")
+        integer_object = integer_class.get_method("valueOf", JavaPrimitiveType.INT).invoke_static(12)
+        print(integer_class.get_field("value").get(integer_object))
+        print(matilda_process.java.find_class("java.util.ArrayList").get_fields())
