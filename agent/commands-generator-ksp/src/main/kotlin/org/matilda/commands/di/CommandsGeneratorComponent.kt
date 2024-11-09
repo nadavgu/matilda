3cf0311ce6dd9f847884a951b8ad20f4d20a4d0b
nadavgu <nadavgu@gmail.com>
2024-11-09 17:21:01 +0200

    Removed commands-generator-ksp

--- agent/commands-generator-ksp/src/main/kotlin/org/matilda/commands/di/CommandsGeneratorComponent.kt
+++
@@ -1,9 +0,0 @@
-package org.matilda.commands.di
-
-import org.matilda.commands.CommandsGeneratingSymbolProcessor
-import dagger.Component
-
-@Component
-interface CommandsGeneratorComponent {
-    fun symbolProcessor(): CommandsGeneratingSymbolProcessor
-}
