c42dc5e05ae353c54745cbb895983bd6eeb494b0
nadavgu <nadavgu@gmail.com>
2025-01-10 20:42:43 +0200

    Writing uncaught exceptions to Stderr in native

--- agent/library/src/nativeMain/kotlin/org/matilda/Main.kt
+++ agent/library/src/nativeMain/kotlin/org/matilda/Main.kt
@@ -7,8 +7,16 @@ import org.matilda.logger.StderrLogger
 import platform.posix.*
 
 fun main() {
-    pingToLoader()
-    MatildaAgent(MatildaConnection(STDIN_FILENO.fdSource(), STDOUT_FILENO.fdSink()), StderrLogger()).run()
+    val logger = StderrLogger()
+    try {
+        pingToLoader()
+        MatildaAgent(MatildaConnection(STDIN_FILENO.fdSource(), STDOUT_FILENO.fdSink()), logger).run()
+    } catch (e: Throwable) {
+        // Handle exception here on purpose, cause kotlin-native's uncaught exception handler writes to stdout,
+        // but we don't want to write there because we use it
+        logger.log("Error running Matilda agent", e)
+        exit(-1)
+    }
 }
 
 @OptIn(ExperimentalForeignApi::class)
