fa078859fdfd549d4ec8c7f29b8eb9d701946811
nadavgu <nadavgu@gmail.com>
2025-01-10 15:12:44 +0200

    Using kotlin AutoClosable instead of java Closable

--- agent/library/src/main/java/org/matilda/commands/listener/CommandResponseListeningInstance.kt
+++ agent/library/src/main/java/org/matilda/commands/listener/CommandResponseListeningInstance.kt
@@ -4,9 +4,8 @@ import org.matilda.commands.CommandFailedException
 import org.matilda.commands.protobuf.CommandResponse
 import org.matilda.messages.listener.MessageListeningInstance
 import pbandk.decodeFromByteArray
-import java.io.Closeable
 
-class CommandResponseListeningInstance(private val mMessageListeningInstance: MessageListeningInstance) : Closeable {
+class CommandResponseListeningInstance(private val mMessageListeningInstance: MessageListeningInstance) : AutoCloseable {
     suspend fun waitForResponse(): ByteArray {
         val message = mMessageListeningInstance.waitForMessage()
         val commandResponse = CommandResponse.decodeFromByteArray(message.data)
--- agent/library/src/main/java/org/matilda/messages/listener/MessageListeningInstance.kt
+++ agent/library/src/main/java/org/matilda/messages/listener/MessageListeningInstance.kt
@@ -3,21 +3,16 @@ package org.matilda.messages.listener
 import kotlinx.coroutines.channels.Channel
 import org.matilda.messages.Message
 import org.matilda.messages.MessageHandlerRegistration
-import java.io.Closeable
 
 class MessageListeningInstance(
     private val mMessageChannel: Channel<Message>,
     private val mRegistration: MessageHandlerRegistration
-) : Closeable {
+) {
     suspend fun waitForMessage(): Message {
         return mMessageChannel.receive()
     }
 
     fun stop() {
         mRegistration.unregister()
-    }
-
-    override fun close() {
-        stop()
     }
 }
