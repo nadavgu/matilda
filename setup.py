e9e65b0efcdc2d5858924f452b8c405d69c24275
nadavgu <nadavgu@gmail.com>
2025-01-11 18:06:40 +0200

    Fixed packaging issues that caused protos and resources to not be included in final installed library

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='matilda',
-      version='0.5.0',
+      version='0.7.0',
       author='Nadav Gur',
       author_email='nadavgu@gmail.com',
       packages=find_packages(include='matilda*'),
+      package_data={'matilda': ['resources/*']},
       install_requires=[
             'maddie @ git+ssh://git@github.com/nadavgu/maddie-python.git@dev',
             'protobuf~=5.28'
