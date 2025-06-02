de631b8233e87dc5adb20ac2bafac466eed8e14c
nadavgu <nadavgu@gmail.com>
2025-06-03 02:57:24 +0300

    Actually loading plugin

--- agent/library/src/nativeMain/kotlin/org/matilda/services/plugins/NativePlugin.kt
+++ agent/library/src/nativeMain/kotlin/org/matilda/services/plugins/NativePlugin.kt
@@ -2,14 +2,19 @@
 
 package org.matilda.services.plugins
 
-import kotlinx.cinterop.CPointed
-import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.*
 import org.matilda.commands.CommandRegistry
 import org.matilda.commands.PluginDependencies
+import org.matilda.plugins.interop.CommandRegistryStruct
+import org.matilda.plugins.interop.PluginDependenciesStruct
+import org.matilda.plugins.interop.moveToCommandRegistry
+import org.matilda.plugins.interop.toPluginDependenciesStruct
 
 class NativePlugin(private val mLibrary: NativeLibrary) : Plugin {
     override fun createCommandRegistry(entryPoint: String, pluginDependencies: PluginDependencies): CommandRegistry {
-        TODO("Not yet implemented")
+        val createCommandRegistryFunction =
+            mLibrary.findFunction<(CValuesRef<PluginDependenciesStruct>) -> CPointer<CommandRegistryStruct>>(entryPoint)
+        val commandRegistryPtr = createCommandRegistryFunction(pluginDependencies.toPluginDependenciesStruct())
+        return commandRegistryPtr.pointed.moveToCommandRegistry()
     }
 }
