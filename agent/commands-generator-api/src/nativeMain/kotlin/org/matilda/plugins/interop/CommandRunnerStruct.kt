e6a83024c957f6df3b41634244c0b1a7638188c6
nadavgu <nadavgu@gmail.com>
2025-06-03 02:11:55 +0300

    Implemented native struct to represent command runner

---
+++ agent/commands-generator-api/src/nativeMain/kotlin/org/matilda/plugins/interop/CommandRunnerStruct.kt
@@ -0,0 +1,19 @@
+@file:OptIn(ExperimentalForeignApi::class)
+package org.matilda.plugins.interop
+
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.StableRef
+import kotlinx.cinterop.asStableRef
+import kotlinx.cinterop.staticCFunction
+import org.matilda.commands.CommandRunner
+
+fun CommandRunnerStruct.toCommandRunner() = NativeCommandRunner(run!!, handle!!)
+
+fun CommandRunnerStruct.initializeFrom(commandRunner: CommandRunner) {
+    val stableRef = StableRef.create(commandRunner)
+    handle = stableRef.asCPointer()
+
+    run = staticCFunction { handle, registryId, commandType, parameterStruct ->
+        handle!!.asStableRef<CommandRunner>().get().run(registryId, commandType, parameterStruct.toByteArray()).toByteArrayStruct()
+    }
+}
---
+++ agent/commands-generator-api/src/nativeMain/kotlin/org/matilda/plugins/interop/NativeCommandRunner.kt
@@ -0,0 +1,16 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
+package org.matilda.plugins.interop
+
+import kotlinx.cinterop.*
+import org.matilda.commands.CommandRunner
+
+class NativeCommandRunner(
+    private val mFunction: CPointer<CFunction<(COpaquePointer?, Int, Int, CValue<ByteArrayStruct>) -> CValue<ByteArrayStruct>>>,
+    private val mInstance: COpaquePointer) : CommandRunner {
+    override fun run(registryId: Int, commandType: Int, parameter: ByteArray): ByteArray {
+        return parameter.tempByteArrayStruct { parameterStruct ->
+            mFunction(mInstance, registryId, commandType, parameterStruct).moveToByteArray()
+        }
+    }
+}
