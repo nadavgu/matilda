e485d692db01579d5a974992dfe3fe4fef0a5fc0
nadavgu <nadavgu@gmail.com>
2025-07-15 17:47:30 +0300

    Configured android plugins in project

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     kotlin("multiplatform") apply false
+    id("com.android.library") apply false
 }
--- agent/settings.gradle.kts
+++ agent/settings.gradle.kts
@@ -2,9 +2,13 @@ pluginManagement {
     plugins {
         val kspVersion: String by settings
         val kotlinVersion: String by settings
+        val androidPluginVersion: String by settings
         id("com.google.devtools.ksp") version kspVersion
         kotlin("jvm") version kotlinVersion
         kotlin("multiplatform") version kotlinVersion
+        id("com.android.application") version androidPluginVersion
+        id("com.android.library") version androidPluginVersion
+        id("org.jetbrains.kotlin.android") version kotlinVersion
     }
     repositories {
         gradlePluginPortal()
