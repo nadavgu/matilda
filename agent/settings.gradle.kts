51af7f3502f805dede2f2cc4c0f85458bb7dd259
nadavgu <nadavgu@gmail.com>
2025-07-17 19:59:32 +0300

    Removed payload - creating payload jar in buildscript in library

--- agent/settings.gradle.kts
+++ agent/settings.gradle.kts
@@ -18,7 +18,6 @@ pluginManagement {
 
 rootProject.name = "Matilda Agent"
 include("library")
-include("payload")
 include("commands-generator")
 include("commands-generator-api")
 include("commands-generator-api-java")
