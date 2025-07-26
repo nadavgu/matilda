e2139c32e47a8e9baf62e3624647ed95f6da3f9f
nadavgu <nadavgu@gmail.com>
2025-07-27 00:00:25 +0300

    Using executable of correct platform for each machine

--- matilda/executable_matilda_runner.py
+++ matilda/executable_matilda_runner.py
@@ -1,12 +1,12 @@
 from matilda.command_matilda_runner import CommandMatildaRunner
 from matilda.platform.matilda_platform import MatildaPlatform
 from matilda.platform.native_matilda_platform import NativeMatildaPlatform
-from matilda.resources.resources import get_resource_path
+from matilda.resources.resources import get_executable_path
 
 
 class ExecutableMatildaRunner(CommandMatildaRunner):
     def __init__(self):
-        super().__init__(get_resource_path("agent-linuxX64.kexe"))
+        super().__init__(get_executable_path(NativeMatildaPlatform.of_this_machine()))
 
     def platform(self) -> MatildaPlatform:
         return NativeMatildaPlatform.of_this_machine()
--- matilda/resources/resources.py
+++ matilda/resources/resources.py
@@ -1,6 +1,10 @@
 import os.path
 from typing import IO
 
+from matilda.platform.architecture import Architecture
+from matilda.platform.native_matilda_platform import NativeMatildaPlatform
+from matilda.platform.operating_system import OperatingSystem
+
 RESOURCES_DIR_PATH = os.path.dirname(__file__)
 
 
@@ -18,3 +22,22 @@ def open_resource(name: str) -> IO:
 def get_resource(name: str) -> bytes:
     with open_resource(name) as f:
         return f.read()
+
+
+def get_executable_path(platform: NativeMatildaPlatform):
+    architecture_part = __get_executable_architecture_part(platform.architecture)
+    os_part = __get_executable_os_part(platform.operating_system)
+    return get_resource_path(f"agent-{os_part}{architecture_part}.kexe")
+
+def __get_executable_architecture_part(architecture: Architecture) -> str:
+    match architecture:
+        case Architecture.X86_64: return "X64"
+        case Architecture.ARM32: return "Arm32"
+        case Architecture.ARM64: return "Arm64"
+    raise ValueError(architecture)
+
+def __get_executable_os_part(operating_system: OperatingSystem) -> str:
+    match operating_system:
+        case OperatingSystem.LINUX: return "linux"
+        case OperatingSystem.ANDROID: return "androidNative"
+    raise ValueError(operating_system)
